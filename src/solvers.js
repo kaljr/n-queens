c70d3954f2f3115a23b59830296b6a771352949f
kaljr <kenny.langston@gmail.com>
2015-04-01 20:42:50 -0700

    Finished through findNRooksSolution

--- src/solvers.js
+++ src/solvers.js
@@ -14,10 +14,27 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+
+  var board = new Board({n:n});
+
+  for(var r=0;r<n;r++) {
+    for(var c=0;c<n;c++) {
+      // place rook
+      var row = board.get(r);
+      row[c] = 1;
+      if(board.hasAnyRooksConflicts()) {
+        row[c] = 0;
+      } else {
+        if(r === n-1) {
+          return board.rows();
+        }
+      }
+    }
+
+  }
+
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
 };
 
 
@@ -48,3 +65,30 @@ window.countNQueensSolutions = function(n) {
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
 };
+
+
+/*
+  var board = new Board({n:4});
+  var solutionArray = [];
+  index = 0;
+  debugger;
+  function recurseQueen(index, testArray) {
+  // push value to first index
+  for (i = 0; i < board.get('n'); i++) {
+
+    while(index < board.get('n')) {
+      var testArray = testArray.slice();
+      testArray[i][index] = 1;
+      if (!board.hasAnyRooksConflicts()) {
+        index++;
+        recurseQueen(index, testArray);
+      }
+    }
+  }
+  // run tests
+
+  // if tests pass invoke function with concatenated array
+};
+
+  recurseQueen(index, board.rows());
+  */
